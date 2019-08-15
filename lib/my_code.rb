94b6432838497577d49c8ca50756b2f07143a3a6
Zac Steinberg <zstein1234@gmail.com>
2019-08-15 21:10:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,4 +18,14 @@ def reduce(array, start = 0)
   return result
 end
 
-
+def reduce_to_any_true(source_array)
+  count = 0
+  while count < source_array.length do
+    current = source_array[count]
+    if current == true 
+      return true
+    end
+    count += 1
+  end
+  return false
+end
